7cabd96d891bbd3ee99bee46db69d89d10c72cfd
João Pedro Barbaresco <89043819+JP-Barbaresco@users.noreply.github.com>
2023-05-13 16:13:21 -0300

    Correção Questão 2

--- Semana8/Questao 2.js
+++ Semana8/Questao 2.js
@@ -4,7 +4,7 @@
 //Dev: João Pedro Camargo Silva Barbaresco
 //DATA: 13/05/2023 
 
-function adicionarItem() {
+function addItem() {
     var input = document.getElementById("item");
     var list = document.getElementById("list");
     var newItem = document.createElement("li");
@@ -12,11 +12,32 @@ function adicionarItem() {
     list.appendChild(newItem);
     input.value = "";
   }
+
+function deleteLastItem() {
+    var input = document.getElementById("item");
+    var list = document.getElementById("list");
+    list.pop()
+}
+
+function deletAllItems() {
+    var input = document.getElementById("item");
+    var list = document.getElementById("list");
+    for (i=0; i < list.length; i++){
+        list.pop()
+    }
+}
   
   //Explicação
+  // Função "addItem":
   // Pega o elemento de input usando o ID "item" e o armazena na variável "input".
   // Pega o elemento de lista usando o ID "list" e o armazena na variável "list".
   // Cria um novo elemento <li> usando a função createElement() e o armazena na variável "newItem".
   // Define o conteúdo do novo elemento usando a propriedade innerText e o valor do input.
   // Adiciona o novo elemento à lista existente usando a função appendChild().
   // Limpa o valor do input atribuindo uma string vazia à propriedade value.
+
+  // Função "deleteLastItem":
+  // Deleta o último ítem da lista com a função "pop()"
+
+  // Funçao "deleteAllItems":
+  // Deleta todos os elementos da lista com um loop for executando "pop()" para cada item da lista
